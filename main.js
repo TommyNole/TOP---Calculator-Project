1dbae1c079bc3aa6e48090fb454e6c7c4a373ab4
TommyNole <tomcastle71@icloud.com>
2023-05-26 17:53:49 -0400

    Add listeners to all buttons and test them

--- main.js
+++ main.js
@@ -1,16 +1,50 @@
-const numberButtons = document.querySelectorAll("[data-number]");
-const operatorButtons = document.querySelectorAll("[data-operator]");
-const equalsButton = document.getElementById("equalsBtn");
-const clearButton = document.getElementById("clearBtn");
-const deleteButton = document.getElementById("deleteBtn");
-const pointButton = document.getElementById("pointBtn");
-const lastOperationScreen = document.getElementById("lastOperationScreen");
-const currentOperationScreen = document.getElementById(
-  "currentOperationScreen"
-);
+const numberButtons = document.querySelectorAll(".number-btn");
+const operatorButtons = document.querySelectorAll(".operator-btn");
+const equalsButton = document.getElementById("equals");
+const clearButton = document.getElementById("clear");
+const deleteButton = document.getElementById("delete-btn");
+const pointButton = document.getElementById("decimal");
+const historyDisplay = document.getElementById("history-display");
+const resultDisplay = document.getElementById("result-display");
 
-window.addEventListener("keydown", handleKeyboardInput);
-equalsButton.addEventListener("click", evaluate);
-clearButton.addEventListener("click", clear);
-deleteButton.addEventListener("click", deleteNumber);
-pointButton.addEventListener("click", appendPoint);
+// window.addEventListener("keydown", handleKeyboardInput);
+// equalsButton.addEventListener("click", evaluate);
+// clearButton.addEventListener("click", clear);
+// deleteButton.addEventListener("click", deleteNumber);
+// deleteButton.addEventListener("click", () => {
+//   console.log(button.textContent);
+// });
+// pointButton.addEventListener("click", appendPoint);
+
+// test event listeners
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.textContent);
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.textContent);
+  });
+});
+
+// test delete button
+deleteButton.addEventListener("click", () => {
+  console.log(deleteButton.textContent);
+});
+
+// test clear button
+clearButton.addEventListener("click", () => {
+  console.log(clearButton.textContent);
+});
+
+// test equals button
+equalsButton.addEventListener("click", () => {
+  console.log(equalsButton.textContent);
+});
+
+// test point button
+pointButton.addEventListener("click", () => {
+  console.log(pointButton.textContent);
+});
